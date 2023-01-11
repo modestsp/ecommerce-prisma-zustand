fb9cf697c7bb681057a48985159215ca6bc2c243
srsebbi <csperichon@gmail.com>
2023-01-11 20:37:12 -0300

    added initial components for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+'use client'
+import styles from './page.module.css'
+
 export default function Home() {
+  // fetch('https://fakestoreapi.com/products')
+  //           .then(res=>res.json())
+  //           .then(json=>console.log(json))
   return (
-    <main>
-      
+    <main className={styles.mainContainer}>
+      <section className={styles.mainHome}>
+        <aside className={styles.featured}>
+          FEATURED
+        </aside>
+        <div className={styles.logo}>
+          <h1>LOGO</h1>
+          <p>explore</p>
+        </div>
+      </section>
+      <section className={styles.offers}>
+        <p>OFFERS</p>
+      </section>
     </main>
   )
 }
