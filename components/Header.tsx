451eb0a8e110c0f28235a749ed12bd26c99e8c0c
srsebbi <csperichon@gmail.com>
2023-01-13 16:27:42 -0300

    Created Header component

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import styles from './components.module.css'
+export const Header  = () => {
+  return (
+    <header className={styles.header}>
+      <p>LOGO</p>
+      <p>SEARCH</p>
+      <p>USERNAME OR LOGIN</p>
+    </header>
+  )
+}
